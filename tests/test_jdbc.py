fe25a14838bbc06a05bf649973f09ae4b6836bde
Marcos <erick.lopez@jokr.it>
2022-10-13 11:27:16 -0500

    [ADD] Unit Test for ODBC

---
+++ tests/test_jdbc.py
@@ -0,0 +1,16 @@
+env_params = dict(
+    account_id="*****",
+    user_email="*****",
+    role_id="*****",
+    dsn="*****",
+    password="*****",
+)
+
+
+def test_make_query_success():
+    from src.NetSuite_Connector.ODBC import ODBC
+
+    nt = ODBC(**env_params)
+    d = nt.query("SELECT * FROM OA_tables")
+    print(d.__dict__)
+    assert d.status == 200
